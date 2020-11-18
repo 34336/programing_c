0e95e2cf8818ef0ba00c346b477d0379783a971e
edwardbas-pl <edwardbas551@interia.pl>
2020-11-18 15:02:14 +0100

    update

--- cwiczenia_4/zad_3.c
+++ cwiczenia_4/zad_3.c
@@ -4,22 +4,66 @@
 int main(){
 	char figura;
 	char z;
-	int a;
+	int h;
 	printf("Podaj, co chcesz narysowac (do wyboru: k=kwadrat, t=trojkat, r=romb): \n");
 	scanf("%c", &figura);
 	printf("Ile razy chcesz uzyc tego znaku: \n");
-	scanf("%d", &a);
-	printf("Podaj znak jakiego program użyje\n");
-	scanf("%c" , &z);
-	if(figura=='k')
-	{
-		
-	}
+	scanf("%d", &h);
+	if(figura=='k'){
+		for ( int i = 0 ; i < h ; i++ ){
+			for( int j = 0 ; j < h ; j ++ ){
+				printf("* ");
+			
+			
+			}
+
+		printf("\n");
+		}
+	}		
+	
 	else if(figura=='t'){
-		printf("trojkat");
+		for( int i = 0 ; i < h ; i ++ ){
+			for(int j = 1 ; j <= 2 * h ; j++){
+				if( j >= h-i && j <= h+i ){
+					printf("*");
+				}
+				else{
+					printf(" ");
+				}
+			}
+
+		printf("\n");
+		}
+
 	}
 	else if(figura=='r'){
-		printf("romb");
+			int h = 7;
+
+	//trojkat
+		for( int i = 0 ; i < h ; i ++ ){
+			for(int j = 1 ; j <= 2 * h ; j++){
+				if( j >= h-i && j <= h+i ){
+					printf("*");
+				}	
+				else{
+					printf(" ");
+				}
+			}
+
+		printf("\n");
+		}
+		for( int i = h - 2 ; i >= 0 ; i -- ){
+			for(int j = 1 ; j <= (2 * h)   ; j++){
+				if( j <= h+i && j >= h-i ){
+					printf("*");
+				}
+				else{
+					printf(" ");
+				}
+			}
+
+		printf("\n");
+		}
 	}
 	else {
 		printf("nie podano figury z puli");
